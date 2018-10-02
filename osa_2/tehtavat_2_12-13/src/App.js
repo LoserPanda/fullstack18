3901864f4f4316483297c1a1ca75f271dd158622
LoserPanda <loserpanda82@gmail.com>
2018-10-03 01:31:33 +0300

    fullstack18/osa2/tehtavat_2_12-13 kokonaisuudessaan valmiit

--- osa_2/tehtavat_2_12-13/src/App.js
+++ osa_2/tehtavat_2_12-13/src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
     super(props);
     this.state = {
       countries: [],
-      filterCountries: ''
+      filterCountries: '',
+      selectedItem: ''
     }
   }
 
@@ -17,7 +18,6 @@ class App extends Component {
     promise
       .then(response => {
         const countries = response.data;
-        console.log(countries);
         this.setState({ countries });
       })
       .catch(error => {
@@ -29,13 +29,26 @@ class App extends Component {
     this.setState({ filterCountries: event.target.value });
   }
 
+  selectItem = (id) => {
+    this.setState({
+      selectedItem: id
+    });
+  }
+
+  selectItem = (id) => () => {
+    this.setState({
+      selectedItem: id
+    });
+  }
+
   render() {
     const countriesToShow =
       this.state.filterCountries === '' ?
         this.state.countries :
         this.state.countries.filter(country => country.name.toLowerCase().includes(this.state.filterCountries.toLowerCase()))
+    
+    const selected = this.state.selectedItem;
 
-    console.log(this.state);
     return (
       <div className="App">
         <FilterCountries filterCountries={this.state.filterCountries} handleFilterChange={this.handleFilterChange} />
@@ -43,7 +56,7 @@ class App extends Component {
           (<p>too many matches, specify another filter</p>) :
           countriesToShow.length === 1 ?
             (countriesToShow.map(country => <Country key={country.name} name={country.name} capital={country.capital} population={country.population} flag={country.flag} />)) :
-            (countriesToShow.map(country => <div key={country.name}>{country.name}</div>))}
+            (countriesToShow.map(country => <div onClick={this.selectItem(country.name)} key={country.name}>{selected === country.name ? <Country key={country.name} name={country.name} capital={country.capital} population={country.population} flag={country.flag} /> : country.name}</div>))}
       </div>
     );
   }
--- osa_2/tehtavat_2_12-13/src/components/Country.js
+++ osa_2/tehtavat_2_12-13/src/components/Country.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Country = ({name, capital, population, flag}) => {
-    return (
-        <div>
-            <h1>{name}</h1>
-            <p>capital: {capital}</p>
-            <p>population: {population}</p>
-            <img src={flag} alt="flag" height={100} />
-        </div>
-    );
-};
+class Country extends Component {
+    render() {
+        return (
+            <div>
+                <h1>{this.props.name}</h1>
+                <p>capital: {this.props.capital}</p>
+                <p>population: {this.props.population}</p>
+                <img src={this.props.flag} alt="flag" height={100} />
+            </div>
+        );
+    }
+}
 
 export default Country;
