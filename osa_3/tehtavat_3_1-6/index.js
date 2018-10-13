97202f114a76d082e34ae40a8a8935ef5d9f36cd
LoserPanda <loserpanda82@gmail.com>
2018-10-13 23:04:05 +0300

    fullstack18/osa3/tehtavat_3_1-6 tehtyinä

---
+++ osa_3/tehtavat_3_1-6/index.js
@@ -0,0 +1,75 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+
+let persons = [
+    {
+        name: "Kalle Kaveri",
+        number: "123456",
+        id: 1
+    },
+    {
+        name: "Kikka Kolmonen",
+        number: "123456",
+        id: 2
+    }
+]
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    res.status(204).end()
+})
+
+const generateId = () => {
+    return Math.floor(Math.random() * (59000 - 2 + 1) + 2)
+}
+
+app.post('/api/persons', (req, res) => {
+    const body = req.body
+
+    if (body.name === undefined || body.number === undefined) {
+        return res.status(400).json({ error: 'name or number missing!!' })
+    }
+
+    if (persons.find(person => person.name == body.name)) {
+        return res.status(400).json({ error: 'name must be unique!!' })
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId()
+    }
+
+    persons = persons.concat(person)
+
+    res.json(person)
+})
+
+app.get('/info', (req, res) => {
+    res.send(`<p>puhelinluettelossa on ${persons.length} henkilön tiedot</p><p>${new Date()}</p>`)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
